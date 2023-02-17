0e64cb3a34c59d433e1c5cee5b332eec3d02adfd
guguspradita <guguspradita19@gmail.com>
2023-02-17 16:59:30 +0700

    modif view blade home,kontak dan produk

--- resources/views/produk.blade.php
+++ resources/views/produk.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman Produk</title>
-</head>
-<body>
-    <h1>Halaman Produk</h1>
-</body>
-</html>
+@extends('FrontEnd.layouts.master')
+
+@section('content')
+    <div class="container">
+        <h1>Halaman Produk</h1>
+    </div>
+@endsection
