33c92e15d5bca6d268449e35d79caa95eead56e5
guguspradita <guguspradita19@gmail.com>
2023-02-22 00:25:01 +0700

    delete view frontEnd Old

--- resources/views/FrontEnd/home.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('FrontEnd.layouts.master')
-
-@section('content')
-    <div class="container">
-        <h1>Halaman Home</h1>
-    </div>
-@endsection
--- resources/views/FrontEnd/kontak.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('FrontEnd.layouts.master')
-@section('title', 'Murahkom Official | Contact-Us')
-
-@section('content')
-    <div class="container">
-        <h1>Halaman Kontak</h1>
-    </div>
-@endsection
