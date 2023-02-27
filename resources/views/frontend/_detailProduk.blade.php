c82ddd67940710bcd4236e8f51c2f020817960b9
Ryan Ardiansyah <ryanardiansyah.person@gmail.com>
2023-02-27 21:05:01 +0700

    create detail product frontend

--- resources/views/frontend/_detailProduk.blade.php
+++ resources/views/frontend/_detailProduk.blade.php
@@ -1,7 +1,75 @@
-@extends('frontend.layouts.main')
+@extends('frontend/layouts/main')
 
 @section('container')
-    <h1>Detail product</h1>
 
-    {{ dd($detail) }}
+<section class="detail">
+    <div class="container">
+        <div class="row row-product">
+            <div class="col-lg-5">
+                <figure class="figure">
+                    <img src="/assets/img/hero-img.png" class="figure-img img-fluid rounded" alt="...">
+                    <figcaption class="figure-caption d-flex justify-content-between">
+                        <a href="">
+                            <img src="/assets/img/about-2.jpg" class="figure-img img-fluid rounded" style="width: 70px" alt="...">
+                        </a>
+                        <a href="">
+                            <img src="/assets/img/about-2.jpg" class="figure-img img-fluid rounded" style="width: 70px" alt="...">
+                        </a>
+                        <a href="">
+                            <img src="/assets/img/about-2.jpg" class="figure-img img-fluid rounded" style="width: 70px" alt="...">
+                        </a>
+                        <a href="">
+                            <img src="/assets/img/about-2.jpg" class="figure-img img-fluid rounded" style="width: 70px" alt="...">
+                        </a>
+                        <a href="">
+                            <img src="/assets/img/about-2.jpg" class="figure-img img-fluid rounded" style="width: 70px" alt="...">
+                        </a>
+                    </figcaption>
+                </figure>
+            </div>
+            
+            <div class="col-lg-7">
+                <h3>Monitor MSI G2712 27 Flat IPS 1080 FHD 170Hz 1ms Anti-glare</h3>
+                <div class="underline"></div>
+                <h4>Rp. 5.000.000</h4>
+                <h6>Note</h6>
+                <ul>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, obcaecati!</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa obcaecati vero temporibus soluta quisquam. Voluptatum.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, obcaecati!</li>
+                    <li>Lorem ipsum dolor sit amet.</li>
+                    <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</li>
+                </ul>
+                <p>Order Via :</p>
+                <a href="#" style="text-decoration: none;">
+                    <img src="https://img.icons8.com/color/48/null/whatsapp--v1.png"/>
+                </a>
+                <a href="#" style="text-decoration: none;">
+                    <img src="https://img.icons8.com/nolan/48/tokopedia.png"/>
+                </a>
+                <a href="#" style="text-decoration: none;">
+                    <img src="https://img.icons8.com/color/48/null/shopee.png"/>
+                </a>
+            </div>
+        </div>
+    
+        <!-- description -->
+        <div class="row row-product">
+            <div class="col-12">
+                <ul class="nav nav-tabs" id="myTab" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link active" id="description-tab" data-bs-toggle="tab" data-bs-target="#description" type="button" role="tab" aria-controls="description" aria-selected="true">Description</button>
+                    </li>
+                </ul>
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="description" role="tabpanel" aria-labelledby="description-tab">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam labore deleniti optio ad soluta, fuga magnam dicta nam corrupti nulla architecto odit mollitia repudiandae ipsum eaque esse ab possimus tenetur eius error doloremque. Veniam illo et esse maiores id neque iure fugiat nulla, debitis porro at consectetur quod aspernatur dolorem nihil? A aspernatur ipsam illum deleniti quibusdam aliquid consequatur eaque laboriosam porro, qui vero eum soluta modi vel? Commodi ipsum, debitis reiciendis animi fuga ex veritatis est optio omnis sequi numquam possimus, iste aut, illum unde voluptates deleniti fugiat adipisci. Debitis aut neque doloremque accusantium quod deserunt quasi quibusdam voluptatibus?</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- end description -->
+    </div>
+</section>
+
 @endsection
--- resources/views/frontend/layouts/main.blade.php
+++ resources/views/frontend/layouts/main.blade.php
@@ -15,14 +15,14 @@
         rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="/assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
     <!-- Template Main CSS File -->
-    <link href="assets/css/main.css" rel="stylesheet">
+    <link href="/assets/css/main.css" rel="stylesheet">
 
 </head>
 
@@ -32,7 +32,6 @@
 
     @yield('container')
 
-
     @include('frontend.partials.footer')
 
 
@@ -40,19 +39,19 @@
         <i class="bi bi-arrow-up-short"></i>
     </a>
 
-    <div id="preloader"></div>
+    {{-- <div id="preloader"></div> --}}
 
         <!-- Vendor JS Files -->
-        <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-        <script src="assets/vendor/aos/aos.js"></script>
-        <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-        <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-        <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-        <script src="assets/vendor/php-email-form/validate.js"></script>
+        <script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="/assets/vendor/aos/aos.js"></script>
+        <script src="/assets/vendor/glightbox/js/glightbox.min.js"></script>
+        <script src="/assets/vendor/purecounter/purecounter_vanilla.js"></script>
+        <script src="/assets/vendor/swiper/swiper-bundle.min.js"></script>
+        <script src="/assets/vendor/php-email-form/validate.js"></script>
 
-        <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-
-        <script src="assets/js/main.js"></script>
+        <script src="/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+        <script src="/assets/js/main.js"></script>
 
 
 </body>
