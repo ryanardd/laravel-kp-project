8477404ff94cfe09588f188710a9b13743fe5ddd
Ryan Ardiansyah <ryanardiansyah.person@gmail.com>
2023-03-15 19:28:35 +0700

    produk setting

--- resources/views/frontend/product.blade.php
+++ resources/views/frontend/product.blade.php
@@ -10,7 +10,7 @@
                 <h2>Kategori</h2>
             </div>
 
-            <div class="search">
+            {{-- <div class="search">
                 <div class="group">
                     <svg class="icon" aria-hidden="true" viewBox="0 0 24 24">
                         <g>
@@ -24,67 +24,55 @@
                             value="{{ request('search') }}">
                     </form>
                 </div>
-            </div>
+            </div> --}}
 
-
+            {{-- @dd($category) --}}
             {{-- @if ($product->count()) --}}
             <div class="portfolio-isotope" data-portfolio-filter="*" data-portfolio-layout="masonry"
                 data-portfolio-sort="original-order" data-aos="fade-up" data-aos-delay="100">
                 <div>
                     <ul class="portfolio-flters">
-                        <li data-filter="*" class="filter-active">All</li>
-                        <li data-filter=".abc">contoh</li>
-                        <li data-filter=".filter-app">Laptop</li>
-                        <li data-filter=".filter-product">PC</li>
+                        @foreach ($category as $cat)
+                            <li data-filter=".filter-product"><a
+                                    href="/category/{{ $cat->slug }}">{{ $cat->nama_category }}</a>
+                            </li>
+                        @endforeach
                     </ul><!-- End Portfolio Filters -->
                 </div>
-
+                {{-- <div class="row gy-3 portfolio-container">
+                    @foreach ($category as $cat)
+                        <div class="col-xl-3 portfolio-item filter-product">
+                            <div class="portfolio-wrap">
+                                <a href="images/cover/" class="glightbox d-flex justify-content-center"><img
+                                        src="images/cover/" class="img-fluid" alt=""></a>
+                                <div class="portfolio-info">
+                                    <h4><a href="portfolio-details.html" title="More Details">{{ $cat->nama_category }}</a>
+                                    </h4>
+                                    <div class="btn btn-dark d-flex justify-content-center mt-2"><a
+                                            href="/category/{{ $cat->slug }}">Read More</a></div>
+                                </div>
+                            </div>
+                        </div><!-- End Portfolio Item -->
+                    @endforeach
+                </div> --}}
+                {{-- <div class="row gy-5 p-2">
+                    @foreach ($category as $cat)
+                        <div class="col-lg-4">
+                            <div class="category p-2">
+                                <a href="" class="">
+                                    <img src="images/cover/" class="menu-img img-fluid" alt="">
+                                </a>
+                                <a href="/product/">
+                                    <h4>{{ $cat->nama_category }}</h4>
+                                </a>
+                                <div class="d-flex justify-content-center mt-2"><a class="btn read-more"
+                                        href="/category/{{ $cat->slug }}">Read
+                                        More</a></div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div> --}}
                 <div class="row gy-3 portfolio-container">
-
-                    @foreach ($all as $row)
-                        <div class="col-xl-3 portfolio-item abc">
-                            <div class="portfolio-wrap">
-                                <a href="images/cover/{{ $row->thumbnail }}" data-gallery="portfolio-gallery-app"
-                                    class="glightbox d-flex justify-content-center"><img
-                                        src="images/cover/{{ $row->thumbnail }}" class="img-fluid" alt=""></a>
-                                <div class="portfolio-info">
-                                    <h4><a href="{{ route('product') }}/{{ $row->slug }}"
-                                            title="More Details">{{ $row->nama_produk }}</a>
-                                    </h4>
-                                    <div class="lead">
-                                        Rp. {{ number_format($row->harga, 0, ',', '.') }}
-                                    </div>
-                                    <p class="d-flex justify-content-start">{{ $row->category->nama_category }}</p>
-                                    <p class="d-flex justify-content-end">{{ $row->stok ? 'Stok tersedia' : '' }}</p>
-                                    <div class="btn btn-dark d-flex justify-content-center mt-2"><a
-                                            href="{{ route('product') }}/{{ $row->slug }}">Read More</a></div>
-                                </div>
-                            </div>
-                        </div><!-- End Portfolio Item -->
-                    @endforeach
-
-                    @foreach ($laptop as $row)
-                        <div class="col-xl-3 portfolio-item filter-app">
-                            <div class="portfolio-wrap">
-                                <a href="images/cover/{{ $row->thumbnail }}" data-gallery="portfolio-gallery-app"
-                                    class="glightbox d-flex justify-content-center"><img
-                                        src="images/cover/{{ $row->thumbnail }}" class="img-fluid" alt=""></a>
-                                <div class="portfolio-info">
-                                    <h4><a href="{{ route('product') }}/{{ $row->slug }}"
-                                            title="More Details">{{ $row->nama_produk }}</a>
-                                    </h4>
-                                    <div class="lead">
-                                        Rp. {{ number_format($row->harga, 0, ',', '.') }}
-                                    </div>
-                                    <p class="d-flex justify-content-start">{{ $row->category->nama_category }}</p>
-                                    <p class="d-flex justify-content-end">{{ $row->stok ? 'Stok tersedia' : '' }}</p>
-                                    <div class="btn btn-dark d-flex justify-content-center mt-2"><a
-                                            href="{{ route('product') }}/{{ $row->slug }}">Read More</a></div>
-                                </div>
-                            </div>
-                        </div><!-- End Portfolio Item -->
-                    @endforeach
-
                     @foreach ($pc as $product)
                         <div class="col-xl-3 portfolio-item filter-product">
                             <div class="portfolio-wrap">
@@ -107,10 +95,6 @@
                         </div><!-- End Portfolio Item -->
                     @endforeach
                 </div><!-- End Portfolio Container -->
-
-                <div class="paginate_button justify-content-end">
-                    {{ $laptop->links() }}
-                </div>
             </div>
             {{-- @else
                 <p class="fs-4 text-center">Not Found</p>
