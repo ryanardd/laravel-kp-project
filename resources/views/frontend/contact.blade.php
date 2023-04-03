9ae04aa2350b923fc1e6ae8df1ec6e3b5fe5e2d9
Ryan Ardiansyah <ryanardiansyah.person@gmail.com>
2023-04-03 20:52:55 +0700

    mmodifikasi view contact

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -11,78 +11,59 @@
                 <h2>Need Help? Contact Us</h2>
             </div>
 
-            <div class="row gx-lg-0 gy-4">
+            <div class="mb-3">
+                {{-- <iframe style="border: 0; width: 100%; height: 350px"
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
+                    frameborder="0" allowfullscreen></iframe> --}}
+                <iframe style="border: 0; width: 100%; height: 350px"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.338527066014!2d112.7446251161769!3d-7.315815759528551!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fb0fcf24f413%3A0x35bdae1c7503cf47!2sMurahkom!5e0!3m2!1sid!2sid!4v1680527558482!5m2!1sid!2sid"
+                    frameborder="0" allowfullscreen></iframe>
+            </div><!-- End Google Maps -->
 
-                <div class="col-lg-4">
-
-                    <div class="info-container d-flex flex-column align-items-center justify-content-center">
-                        <div class="info-item d-flex">
-                            <i class="bi bi-geo-alt flex-shrink-0"></i>
-                            <div>
-                                <h4>Location:</h4>
-                                <p>Plaza Marina Lantai Blok G-05 <br>
-                                    Jl. Margorejo Indah No.97-99, Sidosermo, Kec. Wonocolo, Kota SBY, Jawa Timur
-                                    60238<br><br></p>
-                            </div>
-                        </div><!-- End Info Item -->
-
-                        <div class="info-item d-flex">
-                            <i class="bi bi-envelope flex-shrink-0"></i>
-                            <div>
-                                <h4>Email:</h4>
-                                <p>murahkomofficial@gmail.com</p>
-                            </div>
-                        </div><!-- End Info Item -->
-
-                        <div class="info-item d-flex">
-                            <i class="bi bi-phone flex-shrink-0"></i>
-                            <div>
-                                <h4>Call:</h4>
-                                <p>(031) 8470589</p>
-                            </div>
-                        </div><!-- End Info Item -->
-
-                        <div class="info-item d-flex">
-                            <i class="bi bi-clock flex-shrink-0"></i>
-                            <div>
-                                <h4>Open Hours:</h4>
-                                <p>Senin-Minggu 10.00 - 21.00</p>
-                            </div>
-                        </div><!-- End Info Item -->
+            <div class="row gy-4">
+                <div class="col-md-6">
+                    <div class="info-item d-flex align-items-center">
+                        <i class="icon bi bi-map flex-shrink-0"></i>
+                        <div>
+                            <h3>Our Address</h3>
+                            <p>Plaza Marina Lantai Blok G-05
+                                Jl. Margorejo Indah No.97-99, Sidosermo, Kec. Wonocolo, Kota SBY, Jawa Timur 60238</p>
+                        </div>
                     </div>
+                </div><!-- End Info Item -->
 
-                </div>
+                <div class="col-md-6">
+                    <div class="info-item d-flex align-items-center">
+                        <i class="icon bi bi-envelope flex-shrink-0"></i>
+                        <div>
+                            <h3>Email Us</h3>
+                            <p>murahkomofficial@gmail.com</p>
+                        </div>
+                    </div>
+                </div><!-- End Info Item -->
 
-                <div class="col-lg-8">
-                    <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-                        <div class="row">
-                            <div class="col-md-6 form-group">
-                                <input type="text" name="name" class="form-control" id="name"
-                                    placeholder="Your Name" required>
+                <div class="col-md-6">
+                    <div class="info-item d-flex align-items-center">
+                        <i class="icon bi bi-telephone flex-shrink-0"></i>
+                        <div>
+                            <h3>Call Us</h3>
+                            <p>(031) 8470589</p>
+                        </div>
+                    </div>
+                </div><!-- End Info Item -->
+
+                <div class="col-md-6">
+                    <div class="info-item d-flex align-items-center">
+                        <i class="icon bi bi-share flex-shrink-0"></i>
+                        <div>
+                            <h3>Opening Hours</h3>
+
+                            <div><strong>Senin-Minggu:</strong> 10.00 - 21.00
                             </div>
-                            <div class="col-md-6 form-group mt-md-0 mt-3">
-                                <input type="email" class="form-control" name="email" id="email"
-                                    placeholder="Your Email" required>
-                            </div>
                         </div>
-                        <div class="form-group mt-3">
-                            <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject"
-                                required>
-                        </div>
-                        <div class="form-group mt-3">
-                            <textarea class="form-control" name="message" rows="7" placeholder="Message" required></textarea>
-                        </div>
-                        <div class="my-3">
-                            <div class="loading">Loading</div>
-                            <div class="error-message"></div>
-                            <div class="sent-message">Your message has been sent. Thank you!</div>
-                        </div>
-                        <div class="text-center"><button type="submit">Send Message</button></div>
-                    </form>
-                </div><!-- End Contact Form -->
-
+                    </div>
+                </div><!-- End Info Item -->
             </div>
-
         </div>
     </section><!-- End Contact Section -->
 
